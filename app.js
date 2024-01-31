f1e321b6ca44bd2c99b90a1a688f7a698a1485a5
Abhishek kumar <aky8507049610@gmail.com>
2024-01-31 20:39:13 +0530

    deploy

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(invalidPathHandler);
 app.use(errorResponserHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
  
